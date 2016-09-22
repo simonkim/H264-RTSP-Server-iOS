2d94758e0930126b5d37df12583594f5d4e813f9
Simon Kim <spamhere@kaoma.net>
2016-09-22 13:22:09 +0900

    Fix connection failure after resume from background

--- Encoder Demo/AppDelegate.swift
+++ Encoder Demo/AppDelegate.swift
@@ -42,6 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         
+        CameraServer.server().delegate = captureServiceDelegate
         CameraServer.server().startup()
         let vc = self.window!.rootViewController as! EncoderDemoViewController
         vc.startPreview()
